b4666665c6fdb59f2bd52e4e15a5ea55f9783a76
= <craig@craigshoemaker.net>
2019-02-12 16:08:18 -0500

    Add try/catch

--- batch/index.js
+++ batch/index.js
@@ -3,36 +3,50 @@ const validator = require('../modules/validator.js');
 
 module.exports = async function (context, req) {
 
-    const output = req.query.output;
-    const isValidOutput = renderer.isSupportedType(output);
-    const isValidBatch = !!(req.body);
-    const batch = req.body;
+    try {
 
-    if (isValidOutput && isValidBatch) {
-
-        const isOutputJSON = validator.isJSON(output);
-        const result = validator.validateBatch(batch, output);
-
-        context.res = {
-            status: 200,
-            body: {
-                isValid: true,
-                details: isOutputJSON ? result.details : result
+        const output = req.query.output;
+        const isValidOutput = renderer.isSupportedType(output);
+        const isValidBatch = !!(req.body);
+        const batch = req.body;
+    
+        if (isValidOutput && isValidBatch) {
+    
+            const isOutputJSON = validator.isJSON(output);
+            const result = validator.validateBatch(batch, output);
+    
+            context.res = {
+                status: 200,
+                body: {
+                    isValid: true,
+                    details: isOutputJSON ? result.details : result
+                }
+            };
+    
+            if(isOutputJSON) {
+                context.res.contentType = 'application/json';
             }
-        };
-
-        if(isOutputJSON) {
-            context.res.contentType = 'application/json';
         }
-    }
-    else {
+        else {
+            context.res = {
+                status: 400,
+                body: {
+                    message: 'Please provide a valid render type and article source.',
+                    isValid: isValidOutput && isValidSource,
+                    isValidRenderType: isValidOutput,
+                    isValidSource: isValidSource,
+                }
+            };
+        }
+
+    } catch(ex) {
+        context.log(ex);
+        context.log(JSON.stringify(ex));
         context.res = {
-            status: 400,
+            status: 500,
             body: {
-                message: 'Please provide a valid render type and article source.',
-                isValid: isValidOutput && isValidSource,
-                isValidRenderType: isValidOutput,
-                isValidSource: isValidSource,
+                message: ex.message ? ex.message : 'Internal server error',
+                error: ex
             }
         };
     }
